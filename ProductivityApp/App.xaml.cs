32ca311095b7dfb8229fbbc1a9785cb3ed305ad1
BrainSkel <hanne53rik@gmail.com>
2023-05-09 16:37:15 +0300

    Modified model and Finished Mainpage

--- ProductivityApp/App.xaml.cs
+++ ProductivityApp/App.xaml.cs
@@ -1,15 +1,37 @@
 ﻿using ProductivityApp.Data;
 
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
 namespace ProductivityApp;
 
 public partial class App : Application
 {
+    const int WindowWidth = 540;
+    const int WindowHeight = 1080;
+
     static ChartDatabase database;
     public App()
 	{
 		InitializeComponent();
+#if WINDOWS
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+		{
+            var mauiWindow = handler.VirtualView;
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+        });
+#endif
 
-		MainPage = new AppShell();
+
+        MainPage = new NavigationPage(new MainPage());
 	}
     public static ChartDatabase Database
     {
--- ProductivityApp/MainPage.xaml.cs
+++ ProductivityApp/MainPage.xaml.cs
@@ -35,7 +35,7 @@ public partial class MainPage : ContentPage
 
 
 
-    private void TestPage(System.Object sender, System.EventArgs e)
-        => Application.Current.MainPage = new NavigationPage(new TestPage());
+    //private void TestPage(System.Object sender, System.EventArgs e)
+    //    => Application.Current.MainPage = new NavigationPage(new TestPage());
 }
 
